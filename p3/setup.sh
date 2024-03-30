5a434f5d3b9ceb22828f9efd4c2d43cdcb18477e
Ethan Delage <ethandelage04@gmail.com>
2024-03-30 22:21:44 +0100

    Start part3

---
+++ p3/setup.sh
@@ -0,0 +1,5 @@
+k3d cluster create part3 -c k3d.yaml
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl apply -f myapp.yaml
