1a10ba514d8b34c3390587284b82f5dcc8638ed7
tdameros <tomdamerose@gmail.com>
2024-04-06 18:28:10 +0200

    start refactoring

--- bonus/setup_gitlab_repo.sh
+++ bonus/setup_gitlab_repo.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+source .env.example
+
 GITLAB_TOOLBOX_POD_NAME=$(kubectl get pods -lapp=toolbox -n gitlab -ojsonpath='{.items[0].metadata.name}')
 
-kubectl cp   gitlab_create_user.rb gitlab/$GITLAB_TOOLBOX_POD_NAME:/tmp/test.rb
-kubectl exec -i -n gitlab $GITLAB_TOOLBOX_POD_NAME -- /srv/gitlab/bin/rails runner /tmp/test.rb
+kubectl cp gitlab_create_user.rb gitlab/$GITLAB_TOOLBOX_POD_NAME:/tmp/test.rb
+kubectl exec -i -n gitlab $GITLAB_TOOLBOX_POD_NAME -- /bin/bash -c "\
+        export GITLAB_USERNAME=$GITLAB_USERNAME &&\
+        export GITLAB_EMAIL=$GITLAB_EMAIL &&\
+        export GITLAB_NAME=$GITLAB_NAME &&\
+        export GITLAB_PASSWORD=$GITLAB_PASSWORD &&\
+        export GITLAB_PERSONAL_ACCESS_TOKEN=$GITLAB_PERSONAL_ACCESS_TOKEN &&\
+        /srv/gitlab/bin/rails runner /tmp/test.rb"
 
 JSON_RESPONSE=$(curl -k --request POST \
         --header "PRIVATE-TOKEN: abcd1234" \
