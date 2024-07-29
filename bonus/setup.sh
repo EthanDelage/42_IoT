9fd8a029b3fc4f4b03a252152245d43968e98df7
Tom Damerose <50496792+tdameros@users.noreply.github.com>
2024-07-29 19:36:26 +0200

    Refactor (#3)
    * refactor: bonus
    
    * feat: setup argocd
    
    * fix(wait_for_pods_ready): wait until pods are in ready state rather than running state
    
    * feat: disable gitlab runner
    
    * fix gitlab url
    
    * fix myapp.yaml
    
    ---------
    
    Co-authored-by: EthanDelage <ethandelage04@gmail.com>
    Co-authored-by: edelage <edelage@student.42lyon.fr>

--- bonus/setup.sh
+++ bonus/setup.sh
@@ -1,41 +1,69 @@
 #!/bin/bash
 
+# Function to wait until all pods in a namespace are ready
+wait_for_pods_ready() {
+  local namespace=$1
+  local completed_pods
+  local ready_pods
+  local total_pods
+
+  while true; do
+    completed_pods=$(kubectl get pods -n "$namespace" --field-selector=status.phase==Succeeded -o json | jq -r '.items | length')
+    ready_pods=$(kubectl get pods -n "$namespace" -o json | jq -r '
+  .items[] |
+  select(
+    (.status.containerStatuses | map(select(.ready == true)) | length) == (.status.containerStatuses | length)
+  ) |
+  .metadata.name
+' | wc -l)
+    total_pods=$(kubectl get pods -n "$namespace" -o json | jq -r '.items | length')
+
+    if [ "$(($completed_pods + $ready_pods))" == "$total_pods" ]; then
+      echo "All pods are both Completed and Ready."
+      break
+    else
+      echo "$completed_pods pods have completed and $ready_pods are ready out of $total_pods. Waiting..."
+      sleep 5
+    fi
+  done
+}
+
+# Create k3d cluster
 k3d cluster create bonus -c k3d.yaml
 
+# Create gitlab namespace
 kubectl create namespace gitlab
+
+# Add GitLab Helm repo
 helm repo add gitlab https://charts.gitlab.io/
+
+# Install GitLab using Helm
 helm install gitlab gitlab/gitlab \
   --set global.hosts.domain=example.com \
+  --set global.hosts.externalIP=0.0.0.0 \
+  --set global.hosts.https=false \
+  --set global.hosts.gitlab.https=false \
   --set certmanager-issuer.email=me@example.com \
-  --set gitlab-runner.runners.privileged=true \
+  --set gitlab-runner.install=false \
   --namespace gitlab
 
-while true; do
-  completed_pods=$(kubectl get pods -n gitlab --field-selector=status.phase==Succeeded -o json | jq -r '.items | length')
-  ready_pods=$(kubectl get pods -n gitlab --field-selector=status.phase==Running -o json | jq -r '.items | length')
-  total_pods=$(kubectl get pods -n gitlab -o json | jq -r '.items | length')
-  
-  if [ "$(($completed_pods + $ready_pods))" == "$total_pods" ]; then
-    echo "Tous les pods sont à la fois Completed et Ready."
-    break
-  else
-    echo "$completed_pods pods ont terminé et $ready_pods sont prêts sur un total de $total_pods. Attente..."
-    sleep 5
-  fi
-done
-#kubectl wait --for=condition=Ready pod -n gitlab --all --timeout=600s
+# Wait until all pods in the gitlab namespace are ready
+wait_for_pods_ready "gitlab"
+
+# Display GitLab's initial root password
 kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
 
+# Setup port forwarding
+kubectl port-forward service/gitlab-webservice-default --address 0.0.0.0 -n gitlab 8181:8181 2>&1 >/dev/null &
 
+# Run setup_gitlab_repo.sh script
 bash setup_gitlab_repo.sh
 
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-# kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-
-# kubectl wait --for=condition=Ready pod -n argocd --all
-# kubectl apply -f myapp.yaml
-# argocd admin initial-password -n argocd
-
-
-
+# Uncomment for installing ArgoCD and deploying an application
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+wait_for_pods_ready "argocd"
+kubectl apply -f myapp.yaml
+argocd admin initial-password -n argocd
+kubectl port-forward service/argocd-server --address 0.0.0.0 -n argocd 8080:443 2>&1 >/dev/null &
--- bonus/setup_gitlab_repo.sh
+++ bonus/setup_gitlab_repo.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
+source .env.example
+
 GITLAB_TOOLBOX_POD_NAME=$(kubectl get pods -lapp=toolbox -n gitlab -ojsonpath='{.items[0].metadata.name}')
 
-kubectl cp   gitlab_create_user.rb gitlab/$GITLAB_TOOLBOX_POD_NAME:/tmp/test.rb
-kubectl exec -i -n gitlab $GITLAB_TOOLBOX_POD_NAME -- /srv/gitlab/bin/rails runner /tmp/test.rb
+kubectl cp gitlab_create_user.rb gitlab/$GITLAB_TOOLBOX_POD_NAME:/tmp/gitlab_create_user.rb
+kubectl exec -i -n gitlab $GITLAB_TOOLBOX_POD_NAME -- /bin/bash -c "\
+        export GITLAB_USERNAME=$GITLAB_USERNAME &&\
+        export GITLAB_EMAIL=$GITLAB_EMAIL &&\
+        export GITLAB_NAME=$GITLAB_NAME &&\
+        export GITLAB_PASSWORD=$GITLAB_PASSWORD &&\
+        export GITLAB_PERSONAL_ACCESS_TOKEN=$GITLAB_PERSONAL_ACCESS_TOKEN &&\
+        /srv/gitlab/bin/rails runner /tmp/gitlab_create_user.rb"
 
 JSON_RESPONSE=$(curl -k --request POST \
-        --header "PRIVATE-TOKEN: abcd1234" \
+        --header "PRIVATE-TOKEN: $GITLAB_PERSONAL_ACCESS_TOKEN" \
         --header "Content-Type: application/json" \
-        --data '{"name": "webapp","description": "webapp","path": "webapp","namespace": "webapp","initialize_with_readme": "true"}' \
-        --url "https://gitlab.example.com:1080/api/v4/projects/")
+        --data '{"name": "webapp","description": "webapp","path": "webapp","namespace": "webapp","initialize_with_readme": "false", "visibility": "public"}' \
+        --url "http://localhost:8181/api/v4/projects/")
 
-HTTP_URL=$(echo $JSON_RESPONSE | jq -r '.http_url_to_repo')
+PROJECT_HTTP_URL=$(echo $JSON_RESPONSE | jq -r '.http_url_to_repo')
 SSH_URL=$(echo $JSON_RESPONSE | jq -r '.ssh_url_to_repo')
+
+git config --global user.name $GITLAB_USERNAME
+git config --global user.email $GITLAB_EMAIL
+git config --global init.defaultBranch main
+git config --global http.sslVerify false # Allow self signed certificate
+
+git init --initial-branch=main
+git add .
+git commit -m "feat: Initial commit"
+git push http://$GITLAB_USERNAME:$GITLAB_PERSONAL_ACCESS_TOKEN@localhost:8181/$GITLAB_USERNAME/webapp.git
+
